3a6efaed48f15e44d351624b2355aed6f300c521
Axel Geoffredo <45520521+axelgeoffredo@users.noreply.github.com>
2020-12-01 20:00:19 -0800

    first version of wdd final project

---
+++ assets/scripts/script2.js
@@ -0,0 +1,7 @@
+
+//Create function where losses can be expanded & closed
+let submitbtn = document.getElementById("sub-btn");
+submitbtn.onclick = function() {
+    alert("Your entry has been submitted. Thanks!");
+};
+
